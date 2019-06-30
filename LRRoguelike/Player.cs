59d63eceb8b0b57c04244fd2da9ead07fcba06d7
JBernardoRebelo <bernasfattaker@gmail.com>
2019-07-01 00:24:40 +0100

    Create Player class, initial properties.

---
+++ LRRoguelike/Player.cs
@@ -0,0 +1,25 @@
+﻿
+namespace LRRoguelike
+{
+    /// <summary>
+    /// Player, has a position, HP and moves
+    /// </summary>
+    public class Player
+    {
+        // Properties
+        /// <summary>
+        /// Player's HP, to be decremented in each turn
+        /// </summary>
+        public int HP { get; set; }
+
+        /// <summary>
+        /// Player's X position in map
+        /// </summary>
+        public int Xpos { get; set; }
+
+        /// <summary>
+        /// Player's Y position in map
+        /// </summary>
+        public int Ypos { get; set; }
+    }
+}
