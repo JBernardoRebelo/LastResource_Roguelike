ef09c23b91ade0ac24c2b41e8c0d045e1d8d8258
JBernardoRebelo <bernasfattaker@gmail.com>
2019-07-07 11:32:51 +0100

    Create trap class

---
+++ LRRoguelike/Trap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRRoguelike
+{
+    public class Trap : MapComponents
+    {
+        /// <summary>
+        /// Max damage of trap, between 0 - 100
+        /// </summary>
+        public int MaxDamage { get; set; }
+
+        /// <summary>
+        /// Indidactes if player has fallen into trap
+        /// </summary>
+        public bool FallenInto { get; set; }
+
+        /// <summary>
+        /// Trap constructor, accepts map dimensions and seed for damage
+        /// Assigns position and max Damage
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param> 
+        /// <param name="mD"></param>
+        public Trap(int x, int y, int mD)
+        {
+            Xpos = x;
+            Ypos = y;
+            MaxDamage = mD;
+        }
+    }
+}
