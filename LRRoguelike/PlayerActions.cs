2d4cfe55ecf68e5562ec2c087be6a482a817ed2d
JBernardoRebelo <bernasfattaker@gmail.com>
2019-07-07 11:20:37 +0100

    Optimize fog of war GREATLY

--- LRRoguelike/PlayerActions.cs
+++ LRRoguelike/PlayerActions.cs
@@ -180,66 +180,26 @@ namespace LRRoguelike
         }
 
         /// <summary>
-        /// Show adjacent tiles
+        /// Show adjacent tiles in world
         /// </summary>
         /// <param name="mp"></param>
         public void FogOfWar(List<MapComponents> mapComps, Player player)
         {
+            int distanceX;
+            int distanceY;
+
             foreach (MapComponents mc in mapComps)
             {
-                // Player position
-                if (mc.Xpos == player.Xpos && mc.Ypos == player.Ypos)
-                {
-                    mc.isDisc = true;
-                }
-                // Left to player
-                else if (mc.Xpos == player.Xpos - 1 && mc.Ypos == player.Ypos)
-                {
-                    mc.isDisc = true;
-                }
-                // Right to player
-                else if (mc.Xpos == player.Xpos + 1 && mc.Ypos == player.Ypos)
-                {
-                    mc.isDisc = true;
-                }
-                // Down player
-                else if (mc.Xpos == player.Xpos && mc.Ypos == player.Ypos + 1)
-                {
-                    mc.isDisc = true;
-                }
-                // Up to player
-                else if (mc.Xpos == player.Xpos && mc.Ypos == player.Ypos - 1)
-                {
-                    mc.isDisc = true;
-                }
-                // Up Left
-                else if (mc.Xpos == player.Xpos - 1 &&
-                    mc.Ypos == player.Ypos - 1)
-                {
-                    mc.isDisc = true;
-                }
-                // Up Right
-                else if (mc.Xpos == player.Xpos + 1 &&
-                    mc.Ypos == player.Ypos - 1)
-                {
-                    mc.isDisc = true;
-                }
-                // Down Left
-                else if (mc.Xpos == player.Xpos - 1 && mc.Ypos == player.Ypos + 1)
-                {
-                    mc.isDisc = true;
-                }
-                // Down Right
-                else if (mc.Xpos == player.Xpos + 1 && mc.Ypos == player.Ypos + 1)
-                {
-                    mc.isDisc = true;
-                }
+                // Calculate distances
+                distanceX = player.Xpos - mc.Xpos;
+                distanceY = player.Ypos - mc.Ypos;
 
-                //// Output message saying description and position
-                //if (mc is Exit && !mc.isDisc)
-                //{
-                //    rndr.FoundExit(mc.Xpos, mc.Ypos);
-                //}
+                // Discover adjacent cells to player
+                if (distanceX <= 1 && distanceY <= 1 &&
+                    distanceX >= -1 && distanceY >= -1)
+                {
+                    mc.isDisc = true;
+                }
             }
         }
 
@@ -258,7 +218,7 @@ namespace LRRoguelike
                 distanceX = player.Xpos - mc.Xpos;
                 distanceY = player.Ypos - mc.Ypos;
 
-                if (mc.isDisc && distanceX <= 1 && distanceY <= 1 && 
+                if (mc.isDisc && distanceX <= 1 && distanceY <= 1 &&
                     distanceX >= -1 && distanceY >= -1)
                 {
                     // Show Exit info
@@ -287,13 +247,13 @@ namespace LRRoguelike
         public void PickUpItem
             (Player player, MapItem map, List<MapComponents> mc)
         {
-            if(player.Xpos == map.Xpos || player.Ypos == map.Ypos)
+            if (player.Xpos == map.Xpos || player.Ypos == map.Ypos)
             {
-                if(map.Used)
+                if (map.Used)
                 {
                     Console.WriteLine("I was used so this won't do shit"); // ***********************************
                 }
-                else if(!map.Used)
+                else if (!map.Used)
                 {
                     // Print message
                     rndr.UseMap();
